13ff13e2a70e0f936a87d728f4bc41c4011bfa8f
Ahmed Elalmi <34603230+AhmedAl-Almi@users.noreply.github.com>
2019-05-10 14:08:08 +0300

    Add files via upload

---
+++ precache-manifest.05a72560d20ea91a333d22f8284f2dcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcd386d98d493ef6d92d1e95873ae6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18873b0670a3e30ad613",
+    "url": "/static/css/main.04811a0f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "18873b0670a3e30ad613",
+    "url": "/static/js/main.cbdeb26b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
